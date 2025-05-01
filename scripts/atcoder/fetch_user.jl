16e38c679856f2107c99b9fdd8f317ee99bf29e4
Clare <leeantoyf@gmail.com>
2025-05-01 03:30:05 +0200

    Update fetch_user.jl

--- scripts/atcoder/fetch_user.jl
+++ scripts/atcoder/fetch_user.jl
@@ -65,7 +65,7 @@ function fetch_language()
 end
 
 function submission_list()
-    start = round((Dates.now() - Dates.DateTime(1970, 1, 1, 0, 0, 0)) / 1000)
+    start = round((Dates.now() - Dates.DateTime(1970, 1, 1, 0, 0, 0)), Dates.Second)
     @show start
 end
 
